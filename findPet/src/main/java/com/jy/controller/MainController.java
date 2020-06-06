d503691e733bd992f9a8d1243af2fcf619505456
JongYoung <whddud102@naver.com>
2020-06-06 12:55:30 +0900

    메인 페이지 레이아웃 구성
    footer.html
    - footer 파일 작성
    
    header.html
    - header 파일 작성
    
    home.html
    - 메인 페이지
    
    MainController.java
    - 메인 페이지 컨트롤러 작성

--- findPet/src/main/java/com/jy/controller/MainController.java
+++ findPet/src/main/java/com/jy/controller/MainController.java
@@ -1,15 +1,17 @@
 package com.jy.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainController {
 	
-	@GetMapping("/hello")
-	public String hello() {
-		return "hello, 나는 정상 작동 중";
+	@GetMapping("/home")
+	public void goTo_home () {
 	}
+	
 	
 
 }
