c8af5edc5f82a28a3f5282e751c66c06bf0fbd69
JongYoung <whddud102@naver.com>
2020-06-08 17:00:40 +0900

    MainController 수정
    MainController.java
    - 기본적으로 오늘 유기된 동물의 데이터를 전달하도록 수정

--- findPet/src/main/java/com/jy/controller/MainController.java
+++ findPet/src/main/java/com/jy/controller/MainController.java
@@ -1,15 +1,34 @@
 package com.jy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.jy.domain.pet.SearchingPetItemsDTO;
+import com.jy.utils.API_RequestUtil;
+
+import lombok.extern.java.Log;
+
+@Log
 @Controller
 public class MainController {
 	
+	@Autowired
+	private API_RequestUtil requester;
+	
 	@GetMapping("/home")
-	public void goTo_home () {
+	public void goTo_home (Model model, Integer pageNum) {
+		if(pageNum == null) {
+			pageNum = 1;
+		}
+		
+		SearchingPetItemsDTO result = requester.request_todayAbandoned(pageNum);
+		log.info("result : " + result);
+		
+		
+		model.addAttribute("result", result);
 	}
 	
 	
