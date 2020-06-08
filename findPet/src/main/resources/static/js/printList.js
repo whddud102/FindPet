7d05456ec2746ac7009ea0c92adbbbd0c44f1a68
JongYoung <whddud102@naver.com>
2020-06-08 17:10:19 +0900

    home.html에 ajax 기능 추가
    home.html
    - ajax를 통해 유기동물의 데이터를 받아올 수 있도록 자바스크립트 기능 추가
    - header를 'sticky-top'으로 동작하도록 외부에 <div> 추가
    
    printList.js
    - 유기동물의 데이터를 화면에 출력해주는 함수

---
+++ findPet/src/main/resources/static/js/printList.js
@@ -0,0 +1,48 @@
+function printList(items) {
+	
+		$('#card_row').html("");
+		
+		for(var i = 1; i <= items.length; i++) {
+			// items의 인덱스는 0부터 시작하므로 1을 빼서 가져옴
+			var item = items[i-1];
+			
+			var cardStr = '';
+			
+			cardStr += '<div class="col-xl-6">';
+			cardStr += 		'<div class="card mb-2 border-dark" style="min-width: 18rem; background-color: #FAFAD2;">'; 
+			cardStr += 			'<div class="row no-gutters">';
+			
+			// Card Image
+			cardStr += 				'<div class="col-md-4 d-flex align-items-center justify-content-center">';
+			cardStr +=  				'<a href="' + item.popfile + '" target="_black">';
+			cardStr +=	 					'<img class="card-img img-thumbnail" src=' + item.popfile  + '  alt="이미지 출력 불가">';
+			cardStr +=					'</a>';
+			cardStr += 				'</div>';
+			// Card Image End
+			
+			// Card Body
+			cardStr +=				'<div class="col-md-8 d-flex align-items-center">';
+			cardStr += 					'<div class="card-body pl-0">';
+			cardStr += 						'<ul id="card_info">';
+			cardStr += 							'<li class="m-1">나이 : <span class="font-weight-bold">' + item.age + '</span></li>';
+			cardStr += 							'<li class="m-1">색상 : <span class="font-weight-bold">' + item.colorCd + '</span></li>';
+			cardStr += 							'<li class="m-1">품종 : <span class="font-weight-bold">' + item.kindCd + '</span></li>';
+			cardStr += 							'<li class="m-1">성별 : <span class="font-weight-bold">' + (item.sexCd == 'F' ? '암컷' : '수컷') + '</span></li>';
+			cardStr += 							'<li class="m-1">중성화 여부 : <span class="font-weight-bold">' + (item.neuterYn == 'Y' ? 'O' : 'X') + '</span></li>';
+			cardStr += 							'<li class="m-1">특징 : <span class="font-weight-bold">' + item.specialMark + '</span></li>';
+			cardStr += 							'<li class="m-1">체중 : <span class="font-weight-bold">' + item.weight + '</span></li>';
+			cardStr +=							 '<li class="m-1">발견장소 : <span class="font-weight-bold">' + item.happenPlace + '</span></li>';
+			cardStr += 							'<li class="m-1">관할기관 : <span class="font-weight-bold">' + item.orgNm + '</span></li>';
+			cardStr += 							'<li class="m-1">상태 : <span class="font-weight-bold">' + item.processState + '</span></li>';
+			cardStr +=						'</ul>';
+			cardStr += 					'</div>';
+			cardStr += 				'</div>';
+			// Card Body End
+			
+			cardStr += 			'</div>';
+			cardStr += 		'</div>';
+			cardStr += 	'</div>';
+			
+			$("#card_row").append(cardStr);
+		}
+};
