1960a82e4a91c8c3413c51953b264d00572d1c9f
JongYoung <whddud102@naver.com>
2020-06-09 22:05:20 +0900

    검색 결과 리스트 화면 작성
    list.html
    - 컨트롤러로 부터 검색 옵션값을 전달받아서 input tag에 저장하도록 작성
    
    getURI.js
    - 검색 옵션을 이용해서 api 요청 주소를 동적으로 생성해주는 함수

---
+++ findPet/src/main/resources/static/js/getURI.js
@@ -0,0 +1,39 @@
+function getURI() {
+	// 'yyyy-MM-dd'  -> 'yyyyMMdd' 변환
+	var bgnde = $("#bgnde").val();
+	if (bgnde != '') {
+		var dateArr = bgnde.split('-');
+		bgnde = dateArr[0] + dateArr[1] + dateArr[2];
+	}
+
+	// 'yyyy-MM-dd'  -> 'yyyyMMdd' 변환
+	var endde = $("#endde").val();
+	if (endde != '') {
+		var dateArr = endde.split('-');
+		endde = dateArr[0] + dateArr[1] + dateArr[2];
+	}
+
+	var upkind = $("#upkind").val();
+	var kind = $("#kind").val();
+	var upr_cd = $("#upr_cd").val();
+	var org_cd = $("#org_cd").val();
+	var shelter = $("#shelter").val();
+	var state = $("#state").val();
+	var neuter_yn = $("#neuter_yn").val();
+
+	var currentPage = $("#pageNo").val(); // 현재 보고 있는 페이지 번호를 가진 input Tag
+
+	var URI = '/api/search?';
+	URI += "bgnde=" + bgnde;
+	URI += "&endde=" + endde;
+	URI += "&upkind=" + upkind;
+	URI += "&kind=" + kind;
+	URI += "&upr_cd=" + upr_cd;
+	URI += "&org_cd=" + org_cd;
+	URI += "&shelter=" + shelter;
+	URI += "&state=" + state;
+	URI += "&neuter_yn=" + neuter_yn;
+	URI += "&pageNo=" + currentPage;
+
+	return URI;
+}
