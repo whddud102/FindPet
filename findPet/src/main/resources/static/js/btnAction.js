de7e78852d7f3d22cffe025a008223a15040cd29
JongYoung <whddud102@naver.com>
2020-06-11 12:17:40 +0900

    검색 기간을 필수 옵션으로 변경
    btnAction.js
    - 검색 시작일과 종료일을 필수로 입력하도록 변경
    
    search.html
    - 안내 문구 수정
    
    searchAction.js
    - 시작일, 종료일의 범위를 3개월로 제한

--- findPet/src/main/resources/static/js/btnAction.js
+++ findPet/src/main/resources/static/js/btnAction.js
@@ -1,41 +1,43 @@
-	$(document).ready(function() {
-			
-			$("#searchBtn").on("click", function(e) {
-					e.preventDefault();
-					
-					var bgnde = $("#bgnde").val();
-					var endde = $("#endde").val();
-					
-					var upkind = $("#upkind").val();
-					var kind = $("#kind").val();
-					var upr_cd = $("#upr_cd").val();
-					var org_cd = $("#org_cd").val();
-					var shelter = $("#shelter").val();
-					var state = $("#state").val();
-					var neuter_yn = $("#neuter_yn").val();
-					
-					// 시작, 종료 날짜 모두 선택한 경우
-					if(bgnde != '' && endde != '') {
-						var bgnDate = new Date(bgnde);
-						var endDate = new Date(endde);
-						
-						if(bgnDate > endDate) {
-							alert("시작 날짜는 종료 날짜 보다 이전이어야 합니다");
-							return false;
-						}
-					} 
-					
-					console.log("bgnde  : " + bgnde);
-					console.log("endde  : " + endde);
-					console.log("upkin  : " + upkind);
-					console.log("kind  : " + kind);
-					console.log("upr_cd  : " + upr_cd);
-					console.log("org_cd  : " + org_cd);
-					console.log("shelter  : " +  shelter);
-					console.log("state  : " + state);
-					console.log("neuter_yn  : " + neuter_yn);
-					console.log("==================\n\n");
-					
-					$('#searchForm').submit();
-			});
-		});
+$(document).ready(function() {
+
+	$("#searchBtn").on("click", function(e) {
+		e.preventDefault();
+
+		var bgnde = $("#bgnde").val();
+		var endde = $("#endde").val();
+
+		var upkind = $("#upkind").val();
+		var kind = $("#kind").val();
+		var upr_cd = $("#upr_cd").val();
+		var org_cd = $("#org_cd").val();
+		var shelter = $("#shelter").val();
+		var state = $("#state").val();
+		var neuter_yn = $("#neuter_yn").val();
+
+		if (bgnde == '' || endde == '') {
+			alert("검색 시작일과 종료일을 모두 선택해주세요");
+			return false;
+		}
+
+		var bgnDate = new Date(bgnde);
+		var endDate = new Date(endde);
+
+		if (bgnDate > endDate) {
+			alert("시작일은 종료일 보다 이전이어야 합니다");
+			return false;
+		}
+
+		console.log("bgnde  : " + bgnde);
+		console.log("endde  : " + endde);
+		console.log("upkin  : " + upkind);
+		console.log("kind  : " + kind);
+		console.log("upr_cd  : " + upr_cd);
+		console.log("org_cd  : " + org_cd);
+		console.log("shelter  : " + shelter);
+		console.log("state  : " + state);
+		console.log("neuter_yn  : " + neuter_yn);
+		console.log("==================\n\n");
+
+		$('#searchForm').submit();
+	});
+});
