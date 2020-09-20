a6af22aefa8b1f48c9693de1bc74237d8a9253f9
JongYoung <whddud102@naver.com>
2020-09-20 18:43:27 +0900

    makePagination.js 주석 추가
    makePagination.js
    - 변수의 의미를 잘 알 수 있도록 주석 추가

--- findPet/src/main/resources/static/js/makePagination.js
+++ findPet/src/main/resources/static/js/makePagination.js
@@ -1,7 +1,7 @@
 function makePagination(result) {
-	var currentPage = result.pageNo;
-	var endPage = result.endPage;
-	var current_end_page = Math.ceil(currentPage / 10.0) * 10;
+	var currentPage = result.pageNo;		
+	var endPage = result.endPage;			// 전체 페이지의 끝
+	var current_end_page = Math.ceil(currentPage / 10.0) * 10;	// 현재 보고 있는 10개 페이지의 끝
 	var starPage = current_end_page - 9;
 
 	// 현재 보고 있는 페이지의 끝 보다 전체 페이지의 끝이 더 작으면 현재 페이지의 끝을 전체 페이지의 끝으로 갱신
@@ -11,7 +11,7 @@ function makePagination(result) {
 
 	var li = '';
 
-	// prev가 true라면
+	// prev를 표시해야하는 경우
 	if (starPage != 1) {
 		li += '<li class="page-item"><a class="page-link" href="'
 				+ (starPage - 1) + '">&laquo;</a></li>'
